47406ae26bb7ce7a24d36c8cb091bd62d35a5ee0
Yumacorn <ReachRyanMui@gmail.com>
2021-01-15 13:33:59 -0500

    commit tree 9f8cb7a65ce5b72a2eb019e38250fddb0a7fed96

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,20 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+export default class CoordinatesButton extends Component {
+
+    // callback = (event) => {
+        // let coords = []
+        // console.log(`${props.clientX} ${props.clientY}`)
+        // coords.push([props.clientX, props.clientY])
+    callback = (event) => this.props.onReceiveCoordinates(event)
+        // return (
+        //     this.onReceiveCoordinates([[event.clientX], [event.clientY]])
+        // )
+    // }
+
+    
+    render () {
+        return <button onClick={callback}></button>
+    }
+}
